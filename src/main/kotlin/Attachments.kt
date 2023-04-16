9daeab9f87f0f6b4bb48d998b95baf1dd6bb0f23
sidenko-ve <100142873+sidenko-ve@users.noreply.github.com>
2023-04-16 12:34:07 +0300

    Update Attachments.kt

--- src/main/kotlin/Attachments.kt
+++ src/main/kotlin/Attachments.kt
@@ -2,17 +2,17 @@ interface Attachments {
     val type: String
 }
 
-open class PhotoAttachment : Attachments {
+open class PhotoAttachment(val photo: Photo) : Attachments {
 
     override val type: String = "Photo"
 }
 
-open class VideoAttachment : Attachments {
+open class VideoAttachment(val video: Video) : Attachments {
 
     override val type: String = "Video"
 
 }
 
-data class Photo(var id: Int, val ownerId: Int, val photo: String) : PhotoAttachment()
+data class Photo(var id: Int, val ownerId: Int, val photo: String)
 
-data class Video(var id: Int, val ownerId: Int, val title: String, val duration: Int) : VideoAttachment()
+data class Video(var id: Int, val ownerId: Int, val title: String, val duration: Int)
